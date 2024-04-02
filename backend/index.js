961b525fdae383000c8c0bb27d8d7143e44f00e6
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-04-03 01:03:04 +0530

    created first server

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import profileData from './data/data.js';
+import dotenv from 'dotenv'
+const app = express();
+
+dotenv.config()
+
+//express Api
+app.get('/',(req,res)=>{
+    res.send("Api is working");
+})
+
+
+//Load data using the api
+app.get('/api/data',(req,res)=>{
+    res.send( profileData)
+})
+
+
+//load single data
+app.get('/api/data/:id',(req,res)=>{
+    //console.log(req.params.id)
+    const singleChat = profileData.friends.find((c)=>c.id===parseInt(req.params.id))
+    res.send(singleChat)
+  
+   
+
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+//Server
+app.listen(PORT,console.log(`Server is running on ${PORT}`));
